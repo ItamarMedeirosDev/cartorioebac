59a19f3da22e1ea23d8a65b9d61594de5ca9cce4
Itamar Medeiros <167126040+ItamarMedeirosDev@users.noreply.github.com>
2024-04-15 23:43:24 -0300

    Funcionalidade de adição de senha.
    Acrescentei ao programa a funcionalidade de pedir senha de acesso. Inclui também um while para que, enquanto a senha correta não seja inserida, o sistema continue a enviar uma mensagem de senha incorreta e disponibilizar nova tentativa.

---
+++ cartorio_ebac.cpp
@@ -0,0 +1,337 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //alocação de espaços de memória
+#include <locale.h> //biblioteca de alocaçõ de texto por região
+#include <string.h>// biblioteca responsável por cuidar das strings
+
+int registrar() //função responsável por cadastrar os usuários no sistema.
+	{
+		char arquivo[40];//criação das variáveis que serão utilizadas nessa função ou strings
+		char cpf[40];
+		char nome[40];
+		char sobrenome[40];
+		char email[100];
+		char cargo[40];
+		char continuar;
+	
+	do 
+	
+	{
+		
+		printf("Digite o CPF a ser cadastrado: \n");//coletando informação do usuário
+		scanf("%s", cpf);//armazenamento na string
+		strcpy(arquivo,cpf);//responsável por copiar os valores das strings
+		
+		FILE *file; //cria o arquivo no banco de dados
+		file=fopen(arquivo, "w");//cria o arquivo
+		fprintf(file,cpf);//salva o valor da variável
+		fclose(file);//fecha o arquivo
+		
+		file=fopen (arquivo, "a");//o "a" altera um arquivo que ja existe
+		fprintf(file,",");//acrescenta a virgula dentro do arquivo para separar o cpf do nome, sobrenome e cargo
+		fclose(file);
+		
+		printf("Digite o nome a ser cadastrado: \n");//coletando informação do usuário
+		scanf("%s", nome);//armazenando na string
+		
+		
+		file=fopen(arquivo,"a");//alterando um arquivo que já existe
+		fprintf(file,nome);//salva o valor da variável
+		fclose(file);//fecha o arquivo
+		
+		file=fopen(arquivo, "a");//altera um arquivo que já existe
+		fprintf(file, ",");//acrescenta virgula depois de cada item
+		fclose(file);//fecha o arquivo
+		
+	
+		printf("Digite o sobrenome a ser cadastrado: \n");//coletando informação do usuário
+		scanf("%s", sobrenome);//armazenando na string
+		
+		
+		file=fopen(arquivo,"a");//altera um arquivo que já existe
+		fprintf(file,sobrenome);//salva o valor da variável
+		fclose(file);//fecha o arquivo
+		
+		file=fopen(arquivo, "a");//altera um arquivo que já existe
+		fprintf(file, ",");//acrescenta virgula 
+		fclose(file);//fecha o arquivo
+		
+		printf("Digite o email a ser cadastrado: \n");//coletando informação do usuário
+		fgets(email, sizeof(email), stdin); // Lê uma linha inteira, incluindo espaços em branco		
+		scanf("%s", email);//salva na string
+		
+		file=fopen(arquivo, "a");//altera um arquivo que já existe
+		fprintf(file,email);//salva o valor da variável
+		fclose(file);//fecha o arquivo
+		
+		file=fopen(arquivo,"a");//altera um arquivo que já existe
+		fprintf(file,",");//acrescenta virgula
+		fclose(file);// fecha o arquivo
+		
+		printf("Digite o cargo a ser cadastrado: \n");//coleta informação do usuário
+		scanf("%s", cargo);//salva na string
+		
+		file=fopen(arquivo, "a");// abre e altera um arquivo que já existe
+		fprintf(file,cargo);//salva o valor da variável
+		fclose(file);//fecha o arquivo
+		
+		
+		printf("\n\n*** Usuário cadastrado com sucesso!***\n\n");//mensagem de retorno para o usuário
+		
+		
+		printf("Deseja cadastrar outro usuário? Digite 's' para sim ou 'n' para não\n\n");
+        scanf(" %c", &continuar); // Note o espaço antes de %c para consumir o caractere de nova linha deixado pelo scanf anterior
+        
+    } while (continuar == 's' || continuar == 'S'); // Continua o loop se o usuário digitar 's' ou 'S'
+
+    return 0;
+}
+
+	
+
+int consultar()//inicio das configurações da função consultar
+
+	{
+		setlocale (LC_ALL, "portuguese");//define o idioma como português
+		
+		char cpf[40];//cri as variáveis
+		char conteudo[200];
+		char continuar;
+		
+	do
+		{
+				
+		printf("Digite o CPF a ser consultado: \n");//coleta informação do usuário
+		scanf("%s", cpf);//armazena na string
+		
+		
+		FILE *file;//define arquivo que será aberto
+		file = fopen(cpf,"r");// lê as informações do arquivo
+		
+		if(file == NULL)//inicio de condicionalcaso o arquivo não exista
+		{
+			system("cls");//limpa o sistema
+			printf("XXX CPF não encontrado XXX \n\n");//retorna mensagem para o usuário
+		}
+			
+
+		
+		while(fgets(conteudo, 200, file) != NULL)// cria um loop
+		{
+    printf("\nEssas são as informações do usuário: \n\n");//entrega as informações do usuário consultado
+    
+    		// Separar os campos do conteúdo do arquivo a partir do elemento virgula
+    		char *token = strtok(conteudo, ",");
+    		printf("CPF: %s\n", token);
+
+    		token = strtok(NULL, ",");
+    		printf("Nome: %s\n", token);
+    
+    		token = strtok(NULL, ",");
+    		printf("Sobrenome: %s\n", token);
+
+    		token = strtok(NULL, ",");
+    		printf("Email: %s\n", token);
+
+   		 token = strtok(NULL, ",");
+    		printf("Cargo: %s\n", token);
+
+    		printf("\n");//espaço estético ao fim das informações
+		}
+
+			printf("\nDeseja consultar novo cpf? Digite s para sim ou n para não\n\n");
+			scanf(" %c", &continuar);
+			
+			}
+			
+			while (continuar == 's' || continuar == 'S');
+			return 0;
+			
+}
+	
+	//fecha o arquivo
+	
+		
+
+
+int deletar() //inicio das configurações da função deletar
+	{
+		//define as variáveis
+    	char cpf[40];
+    	char conteudo[200];
+    	char opcao;
+    	char continuar;
+    
+    do
+    
+    {
+	
+
+    	setlocale(LC_ALL, "portuguese");//define idioma como português
+
+    	printf("Digite o CPF a ser deletado: \n\n");//coleta informação do usuário
+    	scanf("%s", cpf);//armazena na string
+
+   	 	FILE *file;// define o arquivo que será aberto
+    	file = fopen(cpf, "r");//abre o arquivo e lê as informações
+
+    	if (file == NULL) //inicio de condição
+		
+		{
+        printf("\n\nCPF não encontrado.\n\n");//devolve mensagem caso não encontre a informação
+        return 0;
+    	}
+
+    while (fgets(conteudo, 200, file) != NULL) //inicia um loop
+	
+	{
+        printf("\nInformações do usuário:\n\n");//devolve informação para o usuário
+        
+       //separa as informações a partir do elemento virgula
+	    char *token = strtok(conteudo, ",");
+        printf("CPF: %s\n", token);
+        token = strtok(NULL, ",");
+        printf("Nome: %s\n", token);
+        token = strtok(NULL, ",");
+        printf("Sobrenome: %s\n", token);
+        token = strtok(NULL, ",");
+        printf("Email: %s\n", token);
+        token = strtok(NULL, ",");
+        printf("Cargo: %s\n", token);
+    }
+	do //inicio da resposta do condicional para quebra o loop
+	{
+
+    	fclose(file);//fecha o arquivo
+
+   		printf("\nDeseja deletar este usuário? Digite 1 para sim ou 2 para não: \n");//coleta informação do usuário
+    	scanf(" %c", &opcao); // armazena a informação e lê apenas um caracter
+
+    if (opcao == '1') // inicio de condicional
+	
+	{
+        if (remove(cpf) == 0) //remove o usuário do banco de dado
+		
+		{ 
+            printf("\n\nUsuário deletado com sucesso.\n\n");//devolutiva para o usuário
+        } 
+		
+		else //alternativa
+		
+		{
+            printf("Erro ao deletar usuário.\n\n");// devolve informação para o usuário
+        }
+        
+    } 
+	
+	else if (opcao == '2') //alternativa
+	
+	{
+        printf("\n\nUsuário não foi deletado.\n\n");//devolve informação para o usuário
+        
+    
+	} else //alternativa
+	
+	{
+        printf("\n\nOpção inválida.\n");//devolve informação para o usuário
+    }
+    
+    while (getchar() != '\n'); // Limpa o buffer de entrada para que mais de 1 caracter não gere mais de 1 resposta
+	}
+	
+	while (opcao != '1' && opcao != '2'); // estabelece um loop
+	
+    
+    printf("\nDeseja deletar outro usuário? Digite s para sim ou n para não\n\n");
+    scanf(" %c", &continuar);
+}
+    while (continuar == 's' || continuar =='S');
+    
+    return 0;
+    
+	}
+
+	
+	
+int main()
+	{
+		int opcao=0; //Definindo as variáveis
+		int loop=1; //criação de nova variável
+		char senha[10]="a";// variável para inserção de senha
+		
+		setlocale (LC_ALL,"portuguese"); //Denifindo Linguagem
+	
+	do
+	{
+			
+		printf ("*** Cartório da EBAC ***\n\n");
+		printf("Login de Administrador\n\nDigite a senha de acesso:\n\n");
+		scanf("%s",senha);
+
+		
+		if(strcmp(senha, "admin")!=0)
+		{
+			printf("\n\nXX Senha incorreta XX\n\n");
+			
+		}
+		
+	}
+		while(strcmp(senha, "admin") != 0);
+		
+				
+		if(strcmp(senha, "admin") == 0)
+
+		
+		{
+			
+			for(loop=1;loop=1;)//Estabelecendo a função de repetição
+			{
+
+				system("cls"); //limpar sistema
+				
+	
+				printf ("*** Cartório da EBAC ***\n\n"); //Inicio do Menu
+				printf ("Digite o número correspondente à opção desejada:\n\n");
+				printf ("\t1. Registrar usuário \n");
+				printf ("\t2. Consultar usuário \n");
+				printf ("\t3. Deletar usuário \n");
+				printf ("\t4. Sair do sistema\n\n");
+				printf ("Opção: ");//Final do Menu	
+					
+			scanf ("%d", &opcao); //Armazenando a escolha do usuário
+			system ("cls");// Sistema "limpa" a tela depois da seleção de opção
+			
+			
+			switch(opcao)//inicio das opções
+				{
+					case 1:
+						registrar();
+						system("pause");
+						break;
+					
+					case 2:
+						consultar();
+						system("pause");
+						break;
+				
+					case 3:
+						deletar();
+						system("pause");
+						break;
+					
+					case 4:
+						printf("\n\n ** Obrigado por utilizar o sistema ** \n\n");
+						return 0;
+						break;
+					
+			
+					default:
+						printf("Opção inválida\n\n");
+						system("pause");
+						break;//fim das opções
+				}
+			}
+		
+		}
+			
+	}	
+
